600bd53879cfe90b371ea2144457e9e1dc2e2e44
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-04 22:58:14 +0300

    add an example

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import products.BaseProduct;
-import products.Decorator.Expirable;
-import products.Decorator.Shippable;
+import products.Expirable;
+import products.ProductFactory;
+import products.Shippable;
+import products.productImpl.BaseProduct;
 import products.Product;
 
 import java.time.LocalDateTime;
@@ -8,24 +9,25 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
 
-        Customer richCustomer = new Customer(1000.0);
-        ShippingService shippingService = new ShippingService(30.0);
-        Cart cart = new Cart(shippingService, richCustomer);
+        Customer customer = new Customer(1000000.0);
+        ShippingService shippingService = new ShippingService(2.0);
+        Cart cart = new Cart(shippingService, customer);
 
-        Product cheese = new BaseProduct("Cheese", 10.0, 5);
+        ProductFactory productFactory = new ProductFactory();
+
+        // Create a shippable product that is expirable
         LocalDateTime expirationDateInFuture = LocalDateTime.now().plusDays(7);
-        cheese = new Expirable(cheese, expirationDateInFuture );
-        cheese = new Shippable(cheese, 0.25);
+        Product cheese = productFactory.createProduct("Cheese", 5.0, 10, expirationDateInFuture, 0.5);
 
+        // Create a shippable product that is not expirable
+        Product tv = productFactory.createProduct("TV", 500.0, 1, 5.0);
 
-        if (cheese instanceof Shippable) {
-            System.out.println("Cheese is shippable with weight: " + ((Shippable) cheese).getWeight() + " kg");
-        }
-        if (cheese instanceof Expirable) {
-            System.out.println("Cheese expires on: " + ((Expirable) cheese).getExpirationDate());
-        }
+        // Create a regular product that is not shippable or expirable
+        Product ebook = productFactory.createProduct("Ebook", 20.0, 3);
 
         cart.add(cheese, 2);
+        cart.add(tv, 1);
+        cart.add(ebook, 3);
 
         cart.checkout();
 
